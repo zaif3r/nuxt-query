201a587f416df56ffd6346f63ce1f3ad0f0a5477
zaifer <zaif3r@pm.me>
2023-01-31 11:30:04 -0500

    chore: nuxi init

---
+++ src/module.ts
@@ -0,0 +1,16 @@
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+
+// Module options TypeScript inteface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: "my-module",
+    configKey: "myModule",
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+  },
+});
